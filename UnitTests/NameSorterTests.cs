346969a2da57d6c7a18834b86270171456ed0608
adamPatrick12 <adam_77paper@hotmail.com>
2023-06-12 15:31:31 -0400

    Initial commit

---
+++ UnitTests/NameSorterTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using UnsortedNames;
+
+namespace Tests
+{
+    [TestFixture]
+    public class NameSorterTests
+    {
+        [Test]
+        public void SortByLastName_ShouldSortNamesAlphabeticallyByLastName()
+        {
+            // Setup unsorted list
+            List<string> names = new List<string>
+            {
+                "Janet Parsons",
+                "Vaughn Lewis",
+                "Adonis Julius Archer",
+                "Shelby Nathan Yoder",
+                "Marin Alvarez",
+                "London Lindsey",
+                "Beau Tristan Bentley",
+                "Leo Gardner",
+                "Hunter Uriah Mathew Clarke",
+                "Mikayla Lopez",
+                "Frankie Conner Ritt"
+            };
+
+            // Setup sorted list
+
+            List<string> expectedSortedNames = new List<string>
+            {
+                "Marin Alvarez",
+                "Adonis Julius Archer",
+                "Beau Tristan Bentley",
+                "Hunter Uriah Mathew Clarke",
+                "Leo Gardner",
+                "Vaughn Lewis",
+                "London Lindsey",
+                "Mikayla Lopez",
+                "Janet Parsons",
+                "Frankie Conner Ritt",
+                "Shelby Nathan Yoder"
+            };
+
+            // Act
+            List<string> sortedNames = NameSorter.SortByLastName(names);
+
+            // Assert
+            Assert.That(sortedNames, Is.EqualTo(expectedSortedNames));
+        }
+
+ 
+    }
+}
---
+++ Unsort_Names/NameSorter.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace UnsortedNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputFilePath = "/Users/adampatrick/Projects/Unsort_Names/Unsort_Names/unsorted-names-list.txt";  //relative paths
+            string outputFilePath = "/Users/adampatrick/Projects/Unsort_Names/Unsort_Names/sorted-names-list.txt";
+
+            try
+            {
+                List<string> names = FileReader.ReadNamesFromFile(inputFilePath);
+                List<string> sortedNames = NameSorter.SortByLastName(names);
+
+                foreach (string name in sortedNames)
+                {
+                    Console.WriteLine(name);
+                }
+
+                FileWriter.WriteNamesToFile(outputFilePath, sortedNames);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    public class FileReader
+    {
+        public static List<string> ReadNamesFromFile(string filePath)
+        {
+            List<string> names = new List<string>();
+
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    names.Add(line);
+                }
+            }
+
+            return names;
+        }
+    }
+
+   public class NameSorter
+    {
+        public static List<string> SortByLastName(List<string> names)
+        {
+            List<string> sortedNames = new List<string>(names);
+
+            sortedNames.Sort((a, b) =>
+            {
+                string[] nameA = a.Split(' ');
+                string[] nameB = b.Split(' ');
+                return string.Compare(nameA[nameA.Length - 1], nameB[nameB.Length - 1]);
+            });
+
+            return sortedNames;
+        }
+    }
+
+    public class FileWriter
+    {
+        public static void WriteNamesToFile(string filePath, List<string> names)
+        {
+            using (StreamWriter sw = new StreamWriter(filePath))
+            {
+                foreach (string name in names)
+                {
+                    sw.WriteLine(name);
+                }
+            }
+        }
+    }
+}
